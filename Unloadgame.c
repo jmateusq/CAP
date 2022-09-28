7d4f959468b697818ccc2cd8534b635cd6ea2500
jmateusq <114442818+jmateusq@users.noreply.github.com>
2022-09-28 17:16:03 -0300

    Update Unloadgame.c

--- Unloadgame.c
+++ Unloadgame.c
@@ -2,5 +2,10 @@
 
 void UnloadGame(void)
 {
-    // TODO: Unload all dynamic loaded data (textures, sounds, models...)
+    UnloadTexture(player.scarfy);
+    UnloadTexture(background.texture_back);
+    UnloadTexture(enemyTexture[0].textureEnemy);
+    UnloadTexture(enemyTexture[1].textureEnemy);
+    UnloadTexture(enemyTexture[2].textureEnemy);
+    UnloadTexture(enemyTexture[3].textureEnemy);
 }
