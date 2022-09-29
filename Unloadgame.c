ce43ca232ee15d7d98beb2512d00e2496eebecaa
allanmoreira001 <114615758+allanmoreira001@users.noreply.github.com>
2022-09-29 07:16:49 -0300

    Update Unloadgame.c

--- Unloadgame.c
+++ Unloadgame.c
@@ -4,8 +4,16 @@ void UnloadGame(void)
     // TODO: Unload all dynamic loaded data (textures, sounds, models...)
     UnloadTexture(player.scarfy);
     UnloadTexture(background.texture_back);
-    UnloadTexture(enemyTexture[0].textureEnemy);
-    UnloadTexture(enemyTexture[1].textureEnemy);
-    UnloadTexture(enemyTexture[2].textureEnemy);
-    UnloadTexture(enemyTexture[3].textureEnemy);
+    UnloadTexture(enemywave1[0].wave1);
+    UnloadTexture(enemywave1[1].wave1);
+    UnloadTexture(enemywave1[2].wave1);
+    UnloadTexture(enemywave1[3].wave1);
+    UnloadTexture(enemywave2[0].wave2);
+    UnloadTexture(enemywave2[1].wave2);
+    UnloadTexture(enemywave2[2].wave2);
+    UnloadTexture(enemywave2[3].wave2);
+    UnloadTexture(enemywave3[0].wave3);
+    UnloadTexture(enemywave3[1].wave3);
+    UnloadTexture(enemywave3[2].wave3);
+    UnloadTexture(enemywave3[3].wave3);
 }
