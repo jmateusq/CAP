f0f03c9d93426e837239ae1b1bf347d1e3b064dd
joaobizineli <114615682+joaobizineli@users.noreply.github.com>
2022-09-29 14:01:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -11,6 +11,17 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "classic game: space invaders");
 
     InitGame();
+    
+    InitAudioDevice();              // Initialize audio device
+
+    Music music = LoadMusicStream("country.mp3");
+
+    PlayMusicStream(music);
+    SetMusicVolume(music, 0.03f);
+
+    float timePlayed = 0.0f;        // Time played normalized
+    bool pause = false;             // Music playing paused
+
 
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
@@ -26,6 +37,20 @@ int main(void)
     {
         // Update and Draw
         //----------------------------------------------------------------------------------
+        UpdateMusicStream(music);   // Update music 
+        if (IsKeyPressed(KEY_P))
+        {
+            pause = !pause;
+
+            if (pause) PauseMusicStream(music);
+            else ResumeMusicStream(music);
+        }
+
+        // Get normalized time played for current music stream
+        timePlayed = GetMusicTimePlayed(music)/GetMusicTimeLength(music);
+
+        if (timePlayed > 1.0f) timePlayed = 1.0f;   // Make sure time played is no longer than music
+        
         UpdateGame();
         DrawGame();
         //----------------------------------------------------------------------------------
@@ -33,6 +58,10 @@ int main(void)
     // De-Initialization
     //--------------------------------------------------------------------------------------
     UnloadGame();         // Unload loaded data (textures, sounds, models...)
+    
+    UnloadMusicStream(music);   // Unload music stream buffers from RAM
+
+    CloseAudioDevice();         // Close audio device (music streaming is automatically stopped)
 
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
