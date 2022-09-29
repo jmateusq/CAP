c7370786bc2a557d6aee9b8012984f5c927e8ec0
allanmoreira001 <114615758+allanmoreira001@users.noreply.github.com>
2022-09-29 07:16:19 -0300

    Update Initgame.c

--- Initgame.c
+++ Initgame.c
@@ -3,7 +3,7 @@
 void InitGame(void)
 {
     //Initialize background
-    background.texture_back = LoadTexture("background.png");
+    background.texture_back = LoadTexture(".\\textures\\background.png");
     background.position_x = 0;
     background.position_y = 0;
 
@@ -25,23 +25,35 @@ void InitGame(void)
     player.rec.width = 40;
     player.rec.height = 40;
     
-    player.scarfy = LoadTexture("New Piskel (1).png");
+    player.scarfy = LoadTexture(".\\textures\\New Piskel (1).png");
     player.speed.x = 4;
     player.speed.y = 5;
     player.color = BLACK;
 
     // Initialize texture enemies
-    enemyTexture[0].textureEnemy = LoadTexture("se.png");
-    enemyTexture[1].textureEnemy = LoadTexture("senao.png");
-    enemyTexture[2].textureEnemy = LoadTexture("leia.png");
-    enemyTexture[3].textureEnemy = LoadTexture("escreva.png");
+    enemywave1[0].wave1 = LoadTexture(".\\textures\\se.png");
+    enemywave1[1].wave1 = LoadTexture(".\\textures\\senao.png");
+    enemywave1[2].wave1 = LoadTexture(".\\textures\\leia.png");
+    enemywave1[3].wave1 = LoadTexture(".\\textures\\escreva.png");
+
+    enemywave2[0].wave2 = LoadTexture(".\\textures\\realloc.png");
+    enemywave2[1].wave2 = LoadTexture(".\\textures\\ponteiro.png");
+    enemywave2[2].wave2 = LoadTexture(".\\textures\\vetor.png");
+    enemywave2[3].wave2 = LoadTexture(".\\textures\\strlen.png");
+
+    enemywave3[0].wave3 = LoadTexture(".\\textures\\0010.png");
+    enemywave3[1].wave3 = LoadTexture(".\\textures\\1001.png");
+    enemywave3[2].wave3 = LoadTexture(".\\textures\\file.png");
+    enemywave3[3].wave3 = LoadTexture(".\\textures\\fseek.png");
 
     // Initializate enemies
     for (int i = 0; i < NUM_MAX_ENEMIES; i++)
     {
-        enemy[i].realTextureEnemy = &enemyTexture[rand() % 4].textureEnemy;
-        enemy[i].rec.width = 20;
-        enemy[i].rec.height = 10;
+        enemy[i].realTextureEnemy[0] = &enemywave1[rand() % 4].wave1;
+        enemy[i].realTextureEnemy[1] = &enemywave2[rand() % 4].wave2;
+        enemy[i].realTextureEnemy[2] = &enemywave3[rand() % 4].wave3;
+        enemy[i].rec.width = 40;
+        enemy[i].rec.height = 13;
         enemy[i].rec.x = GetRandomValue(screenWidth, screenWidth + 1000);
         enemy[i].rec.y = GetRandomValue(0, screenHeight - enemy[i].rec.height);
         enemy[i].speed.x = 5;
